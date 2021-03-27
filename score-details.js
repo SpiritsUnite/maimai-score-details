4e04ccec4ccc4c39a036775e2a97fcb512d6f4fa
Austin Tankiang <austi.tanki@gmail.com>
2021-03-27 22:05:37 +1100

    Try figure out break breakdown
    If no unique solution can be found, fallback to the previous method.

--- score-details.js
+++ score-details.js
@@ -25,12 +25,9 @@ for (let i = 1; i <= 5; i++) {
 const grid = Array.from(table.rows, r => r.innerText.split('\t').map(toInt));
 
 function append(r, c, v) {
-	const p = document.createElement("p");
 	// Safe because v is either hard-coded text or from format_percent() which
 	// only works on numbers.
-	p.innerHTML = v;
-	p.classList.add("maimai-detail-text");
-	table.rows[r].cells[c].appendChild(p);
+	table.rows[r].cells[c].innerHTML += v;
 }
 
 const total = sum(grid[1]) + 2*sum(grid[2]) + 3*sum(grid[3]) + sum(grid[4]) + 5*sum(grid[5]);
@@ -52,38 +49,12 @@ const loss = sum(losses.map(sum));
 for (let r = 1; r <= 5; r++) {
 	for (let c = 3; c <= 5; c++) {
 		const v = losses[r][c];
-		if (v > 0) append(r, c, `(${format_percent(-v)})`);
+		if (v > 0) append(r, c, `<br>(${format_percent(-v)})`);
 	}
 }
 
 const p = parseFloat(document.querySelector('.playlog_achievement_txt').innerText);
-const min_ploss = .25/num_breaks*break_row[2];
-const min_gloss = (5*base/5+.6/num_breaks)*break_row[3];
-const max_ploss = .5/num_breaks*break_row[2];
-const max_gloss = (5*base/2+.6/num_breaks)*break_row[3];
 const rem = 101 - loss - p;
-
-if (break_row[2] > 0) {
-	append(5, 2, (() => {
-		const mi = -Math.max(min_ploss, rem - max_gloss);
-		const ma = -Math.min(max_ploss, rem - min_gloss);
-		if (Math.abs(ma - mi) < 1e-4) {
-			return `(${format_percent(mi)})`;
-		}
-		return `(${format_percent(mi)}~<br>${format_percent(ma)})`;
-	})());
-}
-
-if (break_row[3] > 0) {
-	append(5, 3, (() => {
-		const mi = -Math.max(min_gloss, rem - max_ploss);
-		const ma = -Math.min(max_gloss, rem - min_ploss);
-		if (Math.abs(ma - mi) < 1e-4) {
-			return `(${format_percent(mi)})`;
-		}
-		return `(${format_percent(mi)}~<br>${format_percent(ma)})`;
-	})());
-}
 
 for (let r = 1; r <= 5; r++) {
 	const factor = r == 4 ? 1 : r;
@@ -94,5 +65,67 @@ for (let r = 1; r <= 5; r++) {
 		total += 1;
 	}
 	if (total == 0) continue;
-	append(r, 0, `-${format_percent(v)} (${format_percent(total)})`);
+	append(r, 0, `<br>-${format_percent(v)} (${format_percent(total)})`);
+}
+
+// Try all combinations of breaks to figure out breakdown.
+let closest = null;
+let closest_break = null;
+let next_closest = null;
+for (let gp = 0; gp <= break_row[2]; gp++) {
+for (let gg = 0; gg <= break_row[3]; gg++) {
+for (let mg = 0; mg <= break_row[3] - gg; mg++) {
+	let bp = break_row[2] - gp;
+	let bg = break_row[3] - gg - mg;
+	let bloss = (gp/4 + bp/2)/num_breaks +
+				(5*base/5+.6/num_breaks)*gg +
+				(5*base*.4+.6/num_breaks)*mg +
+				(5*base/2+.6/num_breaks)*bg;
+	if (closest === null || Math.abs(bloss - rem) < closest) {
+		next_closest = closest;
+		closest = Math.abs(bloss - rem);
+		closest_break = [gp, bp, gg, mg, bg];
+	} else if (next_closest === null || Math.abs(bloss - rem) < next_closest) {
+		next_closest = Math.abs(bloss - rem);
+	}
+}
+}
+}
+
+if (next_closest === null || next_closest > 0.00015) {
+	const [gp, bp, gg, mg, bg] = closest_break;
+	const ploss = -(gp/4 + bp/2)/num_breaks;
+	if (break_row[2] > 0) {
+		append(5, 2, `&nbsp;(${gp},${bp})<br>(${format_percent(ploss)})`);
+	}
+	if (break_row[3] > 0) {
+		append(5, 3, `&nbsp;(${gg},${mg},${bg})<br>(${format_percent(-rem - ploss)})`);
+	}
+} else {
+	const min_ploss = .25/num_breaks*break_row[2];
+	const min_gloss = (5*base/5+.6/num_breaks)*break_row[3];
+	const max_ploss = .5/num_breaks*break_row[2];
+	const max_gloss = (5*base/2+.6/num_breaks)*break_row[3];
+
+	if (break_row[2] > 0) {
+		append(5, 2, (() => {
+			const mi = -Math.max(min_ploss, rem - max_gloss);
+			const ma = -Math.min(max_ploss, rem - min_gloss);
+			if (Math.abs(ma - mi) < 1e-4) {
+				return `<br>(${format_percent(mi)})`;
+			}
+			return `<br>(${format_percent(mi)}~<br>${format_percent(ma)})`;
+		})());
+	}
+
+	if (break_row[3] > 0) {
+		append(5, 3, (() => {
+			const mi = -Math.max(min_gloss, rem - max_ploss);
+			const ma = -Math.min(max_gloss, rem - min_ploss);
+			if (Math.abs(ma - mi) < 1e-4) {
+				return `<br>(${format_percent(mi)})`;
+			}
+			return `<br>(${format_percent(mi)}~<br>${format_percent(ma)})`;
+		})());
+	}
 }
